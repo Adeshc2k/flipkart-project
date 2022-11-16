913f3b98e92bc933d416e114b122ce5a51e3f185
Adesh Gawade <117651499+Adeshc2k@users.noreply.github.com>
2022-11-16 11:33:41 +0530

    flipkart project
    done

---
+++ index.js
@@ -0,0 +1,192 @@
+
+// if....else statement
+//
+
+
+// if (x==5) {
+//   document.write("correct");
+// }
+//
+// else if (x<=5) {
+//   document.write("good");
+// }
+//
+// else {
+//   document.write("incoorect")
+// }
+
+
+
+//for loop......
+
+// for (var i = 1; i <=5; i++) {
+// document.write(i);
+// }
+
+
+
+
+//while loop........
+
+// let i=1
+// while (i<=5) {
+//   document.write(i);
+//   i++;
+// }
+
+
+
+//continue....
+// for (let counter=1; counter<=10; counter++) {
+//   if (counter==5) {
+//     continue;
+//   }
+//   document.write(counter);
+// }
+
+
+
+//break....
+// for (let i = 1; i <=10 ; i++) {
+//   if (i==5) {
+//     break;
+//   }
+//   document.write(i);
+// }
+
+// program using switch statement
+// let a = 3;
+//
+// switch (a) {
+//
+//     case 1:
+//         a = 'one';
+//         break;
+//     case 2:
+//         a = 'two';
+//         break;
+//
+//     case 3:
+//       a = "three"
+//       break;
+//     default:
+//         a = 'not found';
+//         break;
+// }
+// document.write(a);
+
+//sum function
+
+// function sum(a,b) {
+//   document.write(a+b +'<br>');
+//
+// }
+// sum(12,12);
+// sum(200,12);
+// sum(300,12);
+// sum(400,12);
+
+//my info
+
+// function myInfo() {
+//   document.write("adesh gawade <br>")
+// }
+// myInfo();
+// myInfo();
+// myInfo();
+// myInfo();
+
+//sum function
+
+// function add(a,b) {
+//   return a+b;
+// }
+// let sum = add(30,20);
+//
+//
+// document.write(sum);
+//
+// function ShowMessage(firstName, lastName) {
+//     document.write("Hello " + firstName + " " + lastName);
+// }
+//
+// ShowMessage("Steve", "Jobs");
+// ShowMessage("Bill", "Gates");
+// ShowMessage(100, 200);
+
+
+//parameter.....
+// function myname( firstName, lastName) {
+//   document.write("hello     " +  firstName  + " "  + lastName + "<br>");
+// }
+// myname("adesh" , "gawade");
+// myname("adesh" , "gawade");
+// myname("adesh" , "gawade");
+
+// function Sum(val1, val2) {
+//     return val1 + val2;
+// };
+//
+// var result = Sum(10,20); // returns 30
+//
+// function Multiply(val1, val2) {
+//     console.log( val1 * val2);
+// };
+//
+// result = Multiply(10,20); // undefined
+
+// let userName = 'John';
+//
+// function showMessage() {
+//   let message = 'Hello, ' + userName;
+//   document.write(message);
+// }
+//
+// // showMessage(); // Hello, John
+//
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return('Do you have permission from your parents?');
+//   }
+// }
+//
+// let age =('How old are you?', 15);
+//
+// if ( checkAge(age) ) {
+//   document.write( 'Access granted' );
+// } else {
+//   document.write( 'Access denied' );
+// }
+// let n = Math.random();
+// alert(n);
+
+// prompt("enter your name");
+// prompt("enter your crush name");
+// let loveScore = Math.random();
+// loveScore = Math.random()*100
+// loveScore = Math.floor(loveScore);
+// alert("your loveScore is =" + loveScore +"%");
+
+// for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+
+// for (var i = 50; i < 100; i++) {
+//   if (i % 10 == 0) console.log("adi");
+// else if (i % 3 == 0) console.log("sonu");
+//   else if (i % 14 == 0) console.log("monu");
+//   else console.log(i);
+//
+//
+//
+//
+//
+// }
+// document.querySelector('#adi').innerHTML = "adesh";
+// document.querySelector('#adi').style.color = "red";
+// document.getElementById('adi').style.backgroundColor = "yellow";
+// document.getElementsByClassName('adu').style.color = "red";
